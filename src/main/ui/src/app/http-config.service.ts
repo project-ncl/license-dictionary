ef387cb15e0acddbfcb140b8ffaf69e040bbf580
Andrea Vibelli <andrea.vibelli@gmail.com>
2018-03-10 13:22:21 +0100

    Formatting

--- src/main/ui/src/app/http-config.service.ts
+++ src/main/ui/src/app/http-config.service.ts
@@ -16,33 +16,33 @@
 /// limitations under the License.
 ///
 
-import {Injectable, Injector} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from "rxjs/Observable";
-import {AuthService} from "./auth.service";
+import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from "rxjs/Observable";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
-  authService: AuthService;
+    authService: AuthService;
 
-  constructor(inj: Injector) {
-    this.authService = inj.get(AuthService);
-  }
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("enhancing req: ", req);
-    let newHeaders = req.headers
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json');
-    if (this.authService.isAuthenticated()) {
-      newHeaders = newHeaders.set("Authorization", "Bearer " + this.authService.getToken());
+    constructor(inj: Injector) {
+        this.authService = inj.get(AuthService);
     }
-    let jsonReq = req.clone(
-      {
-        headers:
-        newHeaders
-      }
-    );
-    console.log("enhanced req: ", jsonReq);
-    return next.handle(jsonReq);
-  }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("enhancing req: ", req);
+        let newHeaders = req.headers
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json');
+        if (this.authService.isAuthenticated()) {
+            newHeaders = newHeaders.set("Authorization", "Bearer " + this.authService.getToken());
+        }
+        let jsonReq = req.clone(
+            {
+                headers:
+                newHeaders
+            }
+        );
+        console.log("enhanced req: ", jsonReq);
+        return next.handle(jsonReq);
+    }
 }
--- src/main/ui/src/main.ts
+++ src/main/ui/src/main.ts
@@ -18,9 +18,9 @@
 
 import 'hammerjs';
 import './polyfills';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
