5a48575d82f9d71cf5edaae1275e804e859294a6
Andrea Vibelli <andrea.vibelli@gmail.com>
2018-04-13 22:30:29 +0200

    Removal of SwaggerDefinition annotation

--- src/main/java/org/jboss/license/dictionary/RestApplication.java
+++ src/main/java/org/jboss/license/dictionary/RestApplication.java
@@ -34,35 +34,11 @@ import org.jboss.license.dictionary.endpoint.ProjectEcosystemEndpoint;
 import org.jboss.license.dictionary.endpoint.ProjectEndpoint;
 import org.jboss.license.dictionary.endpoint.ProjectVersionLicenseEndpoint;
 
-import io.swagger.annotations.Contact;
-import io.swagger.annotations.Info;
-import io.swagger.annotations.License;
-import io.swagger.annotations.SwaggerDefinition;
-
 /**
  * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com <br>
  *         Date: 11/10/17
  */
 @ApplicationPath("/rest")
-@SwaggerDefinition(
-        info = @Info(
-                description = "License Dictionary Database",
-                version = "V1.0.0",
-                title = "License Dictionary API",
-                termsOfService = "",
-                contact = @Contact(
-                   name = "Andrea Vibelli",
-                   email = "andrea.vibelli@gmail.com"
-                ),
-                license = @License(
-                   name = "Apache 2.0",
-                   url = "http://www.apache.org/licenses/LICENSE-2.0"
-                )
-        ),
-        consumes = {"application/json"},
-        produces = {"application/json"},
-        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS}
-)
 public class RestApplication extends Application {
 
     /*
